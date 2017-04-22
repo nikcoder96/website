7652f8d66935e64f49a0e3a994ae297ee2a5a3ca
Nikhil Arora <Nikhil Arora>
2017-04-22 21:58:02 +0530

    First form created

--- music/urls.py
+++ music/urls.py
@@ -1,11 +1,15 @@
 __author__ = 'Nikhil'
 from django.conf.urls import url
 from . import views
-
+app_name = 'music'
 urlpatterns = [
     # /music/
     url(r'^$',views.index,name='index'),
 
-    # /music/712/
+    # /music/<album_id>/
     url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),  #can be more than 10 albums so we add
+
+    # /music/<album_id>/favorite/
+    url(r'^(?P<album_id>[0-9]+)/favorite/$', views.favorite, name='favorite'),
+
 ]
